53f1e35c2f69982561ee151898e277dbafbb76d1
Geovane Clemente <geo.zn13@gmail.com>
2023-11-19 11:47:53 -0300

    renderizando lista do state

--- class-component/src/App.js
+++ class-component/src/App.js
@@ -1,63 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   // bounding, necessário para que a função tenha o state acessível
-  //   this.handlePClick = this.handlePClick.bind(this);
-
-  //   this.state = {
-  //     name: 'Geovane Clemente',
-  //     counter: 0,
-  //   }
-  // }
   state = {
-    name: 'Geovane Clemente',
-    counter: 0,
+    posts: [
+      {
+        id: 1,
+        title: 'O Título 1',
+        body: 'O corpo 1...'
+      },
+      {
+        id: 2,
+        title: 'O Título 2',
+        body: 'O corpo 2...'
+      },
+      {
+        id: 3,
+        title: 'O Título 3',
+        body: 'O corpo 3...'
+      },
+    ]
   }
 
-  // handlePClick() {
-  //   // const { name } = this.state;
-  //   // console.log(`<p> Cliclado, Olá ${name}`)
-  //   this.setState({name: 'Olá Geovane!', counter: 0});    
-  // }
-  
-  // função trocada para arrow, para que seja acessado o this da classe
-  handlePClick = () => {
-    // const { name } = this.state;
-    // console.log(`<p> Cliclado, Olá ${name}`)
-    this.setState({name: 'Olá Geovane!', counter: 0});    
-  }
-
-  handleAClick = (event) => {
-    event.preventDefault();
-    this.setState({counter: this.state.counter + 1})
-  }
-
-  // Método de renderização chamado sempre que ocorre uma mudança no estado
   render() {
-    const { name } = this.state;
-    const { counter } = this.state;
-    
+    const { posts } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePClick}>
-            { name } - { counter }
-          </p>
-          <a
-            onClick={this.handleAClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Este é um Link
-          </a>
-        </header>
+        { posts.map(post => <h1 key={post.id}>{post.title}</h1>) }
       </div>
     );
   }
